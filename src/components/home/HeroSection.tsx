47d9a40b325cb46d961e8d9de2d0df1df3dd99f3
Palash Purkait <palash1998purkait@gmail.com>
2025-06-30 17:29:59 +0530

    test

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -31,7 +31,7 @@ const HeroSection = () => {
       {/* Content */}
       <div className="relative z-10 container mx-auto px-4 md:h-full pt-[20px] md:pt-[60px] text-left ">
         <div className="md:h-full content-center mx-auto max-w-[100%]">
-          <h1 className="md:text-white md:leading-[48px] leading-[30px] mb-4">
+          <h1 className="md:text-white md:leading-[40px] leading-[30px] mb-8">
             <span>Crafting </span>
             <span className="font-bold lg:inline md:block inline">Digital Solutions</span>
             <span className="block"> for your business</span>
