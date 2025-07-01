c804824e2fd6b922545bed1203e8d867c0202ab9
Palash Purkait <palash1998purkait@gmail.com>
2025-07-01 14:52:29 +0530

    all pages chnages design

--- src/components/career/Banner.tsx
+++ src/components/career/Banner.tsx
@@ -39,7 +39,7 @@ const Banner = () => {
               <div className="md:h-full content-center mx-auto max-w-[100%]">
                 <h1 className="md:text-white text-[#2A2A2A] md:leading-[40px] leading-[25px] md:mb-8 mb-4">
                   <span>Work</span>
-                  <span className="font-bold lg:inline md:block inline pl-1">@Bitpastel</span>
+                  <span className="font-bold lg:inline md:block inline pl-2">@ Bitpastel</span>
                   <br/>
                    on Awesome Projects
                 </h1>
