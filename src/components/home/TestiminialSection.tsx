2a0906fc2a3f6fc51da5732dfd7715d0d0c9ac1c
sumcodez <cloud7suman@gmail.com>
2025-07-01 17:24:19 +0530

    fix

--- src/components/home/TestiminialSection.tsx
+++ src/components/home/TestiminialSection.tsx
@@ -17,80 +17,120 @@ interface Testimonial {
 interface TestimonialsSectionProps {
   testimonials?: Testimonial[];
 }
+
 const defaultTestimonials: Testimonial[] = [
   {
     id: '1',
     name: 'Reece Wabara',
     title: 'CEO, Maniére De Voir, UK | Forbes 30 Under 30',
-    message: 'Ayan and his team are superstars, they interpret suggestions well and propose even better and more efficient alternatives, the team\'s work is to a very high standard and turn around time is much faster than a UK equivalent',
+    message:
+      "Ayan and his team are superstars, they interpret suggestions well and propose even better and more efficient alternatives, the team's work is to a very high standard and turn around time is much faster than a UK equivalent",
     image: '/images/img_image_56x56.png',
-    bgColor: 'bg-primary-blue'
+    bgColor: 'bg-[#9FDDEE]',
   },
   {
     id: '2',
-    name: 'Honey Patel',
-    title: 'Business Strategy & Operations Leader, USA',
-    message: 'Bitpastel has been a delight to work with! They take the time to truly understand the requirements and provide expert opinion on design, development, and implementation. The team is also very detail oriented and communicate extremely well. Everything is timely, effective, and well done. I keep going back to them to build new things because there is no one else I would rather work with.',
-    image: '/images/img_image_1.png',
-    bgColor: 'bg-primary-coral'
+    name: 'Cameron Sangster',
+    title: 'Director, RareBoots4U, UK',
+    message:
+      "As soon as I came into contact with Ayan and his Bitpastel team, I felt in safe hands. What impressed me most is Ayan’s level of communication. In the past I have experienced slow turnaround times and lack of communication but with Ayan and his team I genuinely felt like I was their only client and the attention to detail is impeccable. I highly recommend them",
+    image: '/images/img_image_56x53.png',
+    bgColor: 'bg-[#FFEA97]',
   },
   {
     id: '3',
-    name: 'David Jone',
-    title: 'Marketing Manager, Jax Brands, Canada',
-    message: 'Ayan and the Bitpastel team are a reliable and dedicated group. Whenever I reach out to the team about back-end or front-end tasks that need to be completed, they\'re not only quick to respond but I can rely on their turnaround time. Since partnering with them, my mind has been at ease knowing that they will get the job done so that I can focus on other tasks. It has been a pleasure working with them!',
-    image: '/images/img_image_2.png',
-    bgColor: 'bg-primary-yellow'
+    name: 'Honey Patel',
+    title: 'Business Strategy & Operations Leader, USA',
+    message:
+      "Bitpastel has been a delight to work with! They take the time to truly understand the requirements and provide expert opinion on design, development, and implementation. The team is also very detail oriented and communicate extremely well. Everything is timely, effective, and well done. I keep going back to them to build new things because there is no one else I would rather work with.",
+    image: '/images/img_image_1.png',
+    bgColor: 'bg-[#FFA1AA]',
   },
   {
     id: '4',
-    name: 'Cameron Sangster',
-    title: 'Director, RareBoots4U, UK',
-    message: 'As soon as I came into contact with Ayan and his Bitpastel team, I felt in safe hands. What impressed me most is Ayan\'s level of communication. In the past I have experienced slow turnaround times and lack of communication but with Ayan and his team I genuinely felt like I was their only client and the attention to detail is impeccable. I highly recommend them',
-    image: '/images/img_image_56x53.png',
-    bgColor: 'bg-primary-yellow'
+    name: 'Paul R. Turner',
+    title: 'International Director & Co-founder, Food for Life Global, USA',
+    message:
+      'Ayan and his team were excellent to work with. He was a great communicator, took the time to understand my needs and always delivered as promised. I will hire him again in the future.',
+    image: '/images/img_image_51x56.png',
+    bgColor: 'bg-[#82EAB3]',
   },
   {
     id: '5',
-    name: 'Paul R. Turner',
-    title: 'International Director & Co-founder, Food for Life Global, USA',
-    message: 'Ayan and his team were excellent to work with. He was a great communicator, took the time to understand my needs and always delivered as promised. I will hire him again in the future.',
-    image: '/images/img_image_51x56.png',
-    bgColor: 'bg-[#82EAB3]'
+    name: 'David Jones',
+    title: 'Marketing Manager, Jax Brands, Canada',
+    message:
+      "Ayan and the Bitpastel team are a reliable and dedicated group. Whenever I reach out to the team about back-end or front-end tasks that need to be completed, they're not only quick to respond but I can rely on their turnaround time. Since partnering with them, my mind has been at ease knowing that they will get the job done so that I can focus on other tasks. It has been a pleasure working with them!",
+    image: '/images/img_image_2.png',
+    bgColor: 'bg-[#FFEA97]',
   },
   {
     id: '6',
     name: 'Josiah M. Young, Esq.',
     title: 'Managing Shareholder, The Law Office of Josiah Young, USA',
-    message: 'Ayan and his team were very helpful, responsive and knowledgeable. This contract has been completed but I will continue to work with Ayan. I would recommend his services to anyone looking to build a website or improve their online presence. The design was very clean. Ayan was also a pleasure to work with and made the process of completing this task much easier.',
+    message:
+      'Ayan and his team were very helpful, responsive and knowledgeable. This contract has been completed but I will continue to work with Ayan. I would recommend his services to anyone looking to build a website or improve their online presence. The design was very clean. Ayan was also a pleasure to work with and made the process of completing this task much easier.',
     image: '/images/img_image_3.png',
-    bgColor: 'bg-primary-blue'
+    bgColor: 'bg-[#9FDDEE]',
   },
   {
     id: '7',
-    name: 'Cameron Sangster',
-    title: 'Director, RareBoots4U, UK',
-    message: 'As soon as I came into contact with Ayan and his Bitpastel team, I felt in safe hands. What impressed me most is Ayan\'s level of communication. In the past I have experienced slow turnaround times and lack of communication but with Ayan and his team I genuinely felt like I was their only client and the attention to detail is impeccable. I highly recommend them',
-    image: '/images/img_image_56x53.png',
-    bgColor: 'bg-primary-yellow'
+    name: 'Doug Anderson',
+    title: 'President, Blue Fence Real Estate, USA',
+    message:
+      'The entire team did a great job and will definitely hire again for future projects. They delivered exactly what they promised on time and on budget. The attention to detail was perfect as was the constant communication. We are happy we finally found a great team to work with',
+    image: '/images/Doug_A.jpg',
+    bgColor: 'bg-[#FFA1AA]',
   },
   {
     id: '8',
-    name: 'Paul R. Turner',
-    title: 'International Director & Co-founder, Food for Life Global, USA',
-    message: 'Ayan and his team were excellent to work with. He was a great communicator, took the time to understand my needs and always delivered as promised. I will hire him again in the future.',
-    image: '/images/img_image_51x56.png',
-    bgColor: 'bg-[#82EAB3]'
+    name: 'Anthony Deketelaere',
+    title: 'Founder, YesMLS, Belgium',
+    message:
+      'The people at Bitpastel are professionals, the SRS (Software Requirement Specification) that the team is working on is the exact translation of my insights and idea\'s about the app. Bitpastel gives me the guarantee that the app will be all I wanted it to be, and more... I highly recommend them for your next project',
+    image: '/images/Anthony.jpg',
+    bgColor: 'bg-[#FFEA97]',
   },
   {
     id: '9',
-    name: 'Josiah M. Young, Esq.',
-    title: 'Managing Shareholder, The Law Office of Josiah Young, USA',
-    message: 'Ayan and his team were very helpful, responsive and knowledgeable. This contract has been completed but I will continue to work with Ayan. I would recommend his services to anyone looking to build a website or improve their online presence. The design was very clean. Ayan was also a pleasure to work with and made the process of completing this task much easier.',
-    image: '/images/img_image_3.png',
-    bgColor: 'bg-primary-blue'
+    name: 'Brandon Philbrick',
+    title: 'Brand Manager & Designer, USA',
+    message:
+      'Bitpastel has been doing custom HubSpot COS development for us and our expectations have been consistently exceeded. There is always a great turnaround on project requests and they are available for talking through upcoming work and ready to take on new challenges.',
+    image: '/images/Brandon_P.jpg',
+    bgColor: 'bg-[#82EAB3]',
+  },
+  {
+    id: '10',
+    name: 'Dan Miller',
+    title: 'CEO, Young Professionals, UK',
+    message:
+      "We have worked with Bitpastel now for over 2 years and we've been really happy with the level of service and development work we have had done. They now manage all of our technology needs on an ongoing basis and nothing ever seems to be an issue with our requirements. Work is completed on time and to the highest quality that meets our expectations.",
+    image: '/images/Dan_M.jpg',
+    bgColor: 'bg-[#9FDDEE]',
+  },
+  {
+    id: '11',
+    name: 'Charles Fried',
+    title: 'CTO & Co-Founder, BlockSmith Capital Ltd, UK',
+    message:
+      'Ayan is a sharp individual and showed a very good ability to quickly understand our brief and get his team to deliver according to our specification. I recommend',
+    image: '/images/Charles_F.jpg',
+    bgColor: 'bg-[#FFA1AA]',
+  },
+  {
+    id: '12',
+    name: 'Alf Bergin',
+    title: 'CEO, Svart på Kvitt SA, Norway',
+    message:
+      'Bitpastel is an ambitious design & development team. They take good care of the client and have a professional attitude. What I really appreciate is that work is done quickly and without delays which is very important for a long-term business relationship. I really like their way to communicate',
+    image: '/images/Alf_B.jpeg',
+    bgColor: 'bg-[#82EAB3]',
   }
 ];
+
+
+
 
 // const TestimonialsSection: React.FC<TestimonialsSectionProps> = ({
 //   testimonials = defaultTestimonials,
