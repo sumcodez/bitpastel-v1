433404b4bd9cf16f2674c1c025dd4e6f48f721e3
Palash Purkait <palash1998purkait@gmail.com>
2025-07-03 13:05:12 +0530

    all pages chnages design

--- src/components/team/TeamEngage.tsx
+++ src/components/team/TeamEngage.tsx
@@ -9,28 +9,28 @@ const TeamEngage = () => {
         <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 ">
           <div className="rounded-tr-[40px] rounded-br-[40px] overflow-hidden lg:mr-0 md:mr-4 mr-4 min-h-[15rem] md:min-h-full">
                       <Image
-                        src="/images/person-client.png"
+                        src="/images/team_member_banner.webp"
                         alt="Team collaboration"
                         width={711}
                         height={406}
-                        className="w-full md:h-full h-[280px] md:object-unset object-cover md:object-center object-right"
+                        className="w-full md:h-[440px] h-[280px] md:object-unset object-cover md:object-[50%_17%] object-right"
                       />
                    
           </div>
-          <div className="lg:ml-0 md:ml-4 ml-4 bg-primary-mint lg:p-12 md:p-12 py-10 px-6 rounded-tl-[40px] rounded-bl-[40px] content-center overflow-hidden">
+          <div className="lg:ml-0 md:ml-4 ml-4 bg-primary-mint lg:pl-[80px] md:pl-[60px] md:pr-[40px] py-8 px-8 rounded-tl-[40px] rounded-bl-[40px] content-center overflow-hidden">
             {/* <div className="lg:w-1/2 bg-primary-mint rounded-[40px] p-12"> */}
-            <h2 className=" md:leading-[45px] leading-[24px] font-source md:font-[600] font-bold title md:text-[35px] title text-title md:mb-8 mb-4 ">
+            <h2 className=" md:leading-[40px] leading-[normal] font-source md:font-[600] font-bold title title text-title md:mb-[20px] mb-4">
               Work on awesome Ideas &
               <br />
               Technologies
             </h2>
-            <div className='max-w-[520px]'>
-              <p className="md:leading-[36px] leading-[30px] text-[18px]">
+            <div className='max-w-[500px]'>
+              <p className="md:leading-[30px] leading-[20px] text-[16px] text-[#212529]">
                 At Bitpastel, we create with curiosity. We continue to go forward with an inclusive
                 view for creating equal opportunities for all our employees.
               </p>
-              <p className="md:leading-[36px] leading-[30px] text-[18px]">
-                Visit our <button className="inline-block">Careers</button> page to find out about
+              <p className="md:leading-[30px] leading-[30px] text-[#212529]">
+                Visit our <button className="inline-block underline">Careers</button> page to find out about
                 open positions.
               </p>
               <button className="btn font-inter mt-6 h-auto font-[400] bg-green-btn">
