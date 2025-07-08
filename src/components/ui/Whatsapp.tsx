2c0ea3a35a7da1359e2339166dae8ba6986e7548
sumcodez <cloud7suman@gmail.com>
2025-07-08 13:17:46 +0530

    real time chat app

--- src/components/ui/Whatsapp.tsx
+++ src/components/ui/Whatsapp.tsx
@@ -5,7 +5,7 @@ const whatsapp = () => {
   return (
     <>
      <div className='fixed bottom-[146px] right-[27px] z-50'>
-       <Link href={"/"}>
+       <Link href={"https://api.whatsapp.com/send/?phone=919830566248&text=Hey...+I+would+like+to+have+a+quick+chat+with+you.&type=phone_number&app_absent=0"}>
              <Image
              src={"/images/whatsapp_dropshadow.png"}
              width={55}
