1bbcf31f6abf55da65bdc074a4ea6d9292b1f9e9
Palash Purkait <palash1998purkait@gmail.com>
2025-07-10 19:38:29 +0530

    all pages chnages design

--- src/components/home/ServicesSection.tsx
+++ src/components/home/ServicesSection.tsx
@@ -7,7 +7,6 @@ interface services{
   title: string;
   description: string;
   icon: string;
-  mobileIcon: string;
   bgColor: string;
   bgcolorCollapsed: string;
   mobileTextColor: string;
@@ -17,6 +16,7 @@ interface services{
 const ServicesSection = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
   const [isMobile, setisMobile] = useState(false); 
+
 
   useEffect(() => {
   const handleResize = () => {
@@ -28,6 +28,9 @@ const ServicesSection = () => {
     window.removeEventListener('resize', handleResize);
   };
 })
+
+
+
   const handleClick = (index: number) => {
     setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
   }; 
@@ -37,20 +40,18 @@ const ServicesSection = () => {
       title: 'Artificial Intelligence',
       description: 'Boost efficiency and automate workflows with intelligent apps',
       icon: '/images/AI icon 1.svg',
-      mobileIcon:'/images/ai-icon-yellow.png',
       bgColor: '  bg-primary-green-light',
-      bgcolorCollapsed:'#d8f5e9',
-      mobileTextColor: " text-[rgba(62,203,144,1)]",
+      bgcolorCollapsed:'#fcf4d7',
+      mobileTextColor: " text-[rgba(240,198,56,1)]",
     },
     {
       id: 'shopify',
       title: 'Shopify Plus',
       description: 'Customised Shopify Plus design, development, and app solutions',
       icon: '/images/img_hugeiconsshopify.svg',
-      mobileIcon:'/images/shopify-green-icon.png',
       bgColor: 'bg-primary-yellow-light',
-      bgcolorCollapsed: '#fcf4d7',
-      mobileTextColor: "text-[rgba(240,198,56,1)]",
+      bgcolorCollapsed: '#d8f5e9',
+      mobileTextColor: "text-[rgba(62,203,144,1)]",
       
     },
     {
@@ -58,7 +59,6 @@ const ServicesSection = () => {
       title: 'Web Development',
       description: 'Custom websites built with React, Angular, PHP, WordPress, Webflow',
       icon: '/images/img_icoutlinelaptop.svg',
-      mobileIcon:'/images/img_icoutlinelaptop.svg',
       bgColor: 'bg-primary-coral-light',
       bgcolorCollapsed:'#fde3e5',
        mobileTextColor: "text-[rgba(245,116,126,1)]",
@@ -68,7 +68,6 @@ const ServicesSection = () => {
       title: 'Design & Creatives',
       description: 'Crafting creative visuals for marketing and brand identity',
       icon: '/images/img_iconoirdesignnib.svg',
-      mobileIcon: '/images/img_iconoirdesignnib.svg',
       bgColor: 'bg-primary-yellow-light',
       bgcolorCollapsed:'#fcf4d7',
       mobileTextColor: "text-[rgba(240,198,56,1)]",
@@ -78,7 +77,6 @@ const ServicesSection = () => {
       title: 'E-commerce',
       description: 'Bespoke e-commerce platforms that fuel your brand’s expansion',
       icon: '/images/img_bicartplus.svg',
-      mobileIcon:'/images/img_bicartplus.svg',
       bgColor: 'bg-primary-coral-light',
       bgcolorCollapsed:'#fde3e5',
       mobileTextColor: "text-[rgba(245,116,126,1)]",
@@ -88,19 +86,20 @@ const ServicesSection = () => {
       title: 'Custom SaaS Development',
       description: 'Flexible SaaS Applications focused on achieving your business vision',
       icon: '/images/img_lineiconscloudiot2.svg',
-      mobileIcon:'/images/img_lineiconscloudiot2.svg',
       bgColor: 'bg-primary-green-light',
       bgcolorCollapsed:'#d8f5e9',
       mobileTextColor: "text-[rgba(62,203,144,1)]",
     },
   ];
+
+
+
       const servicesMobile:services[] = [
     {
       id: 'ai',
       title: 'Artificial Intelligence',
       description: 'Boost efficiency and automate workflows with intelligent apps',
-      icon: '/images/AI icon 1.svg',
-      mobileIcon:'/images/ai-icon-yellow.png',
+      icon: ' /images/img_hugeiconsshopify.svg',
       bgColor: ' bg-primary-yellow-light',
       bgcolorCollapsed:'#fcf4d7',
       mobileTextColor: " text-[rgba(240,198,56,1)]",
@@ -109,8 +108,7 @@ const ServicesSection = () => {
       id: 'shopify',
       title: 'Shopify Plus',
       description: 'Customised Shopify Plus design, development, and app solutions',
-      icon: '/images/img_hugeiconsshopify.svg',
-      mobileIcon:'/images/shopify-green-icon.png',
+      icon: '/images/AI icon 1.svg',
       bgColor: 'bg-primary-green-light',
       bgcolorCollapsed: '#d8f5e9',
       mobileTextColor: "text-[rgba(62,203,144,1)]",
@@ -121,7 +119,6 @@ const ServicesSection = () => {
       title: 'Web Development',
       description: 'Custom websites built with React, Angular, PHP, WordPress, Webflow',
       icon: '/images/img_icoutlinelaptop.svg',
-      mobileIcon:'/images/img_icoutlinelaptop.svg',
       bgColor: 'bg-primary-coral-light',
       bgcolorCollapsed:'#fde3e5',
        mobileTextColor: "text-[rgba(245,116,126,1)]",
@@ -131,7 +128,6 @@ const ServicesSection = () => {
       title: 'Design & Creatives',
       description: 'Crafting creative visuals for marketing and brand identity',
       icon: '/images/img_iconoirdesignnib.svg',
-      mobileIcon: '/images/img_iconoirdesignnib.svg',
       bgColor: 'bg-primary-yellow-light',
       bgcolorCollapsed:'#fcf4d7',
       mobileTextColor: "text-[rgba(240,198,56,1)]",
@@ -141,33 +137,34 @@ const ServicesSection = () => {
       title: 'Custom SaaS Development',
       description: 'Flexible SaaS Applications focused on achieving your business vision',
       icon: '/images/img_lineiconscloudiot2.svg',
-      mobileIcon:'/images/img_lineiconscloudiot2.svg',
       bgColor: 'bg-primary-green-light',
       bgcolorCollapsed:'#d8f5e9',
       mobileTextColor: "text-[rgba(62,203,144,1)]",
+
     },
     {
       id: 'ecommerce',
       title: 'E-commerce',
       description: 'Bespoke e-commerce platforms that fuel your brand’s expansion',
       icon: '/images/img_bicartplus.svg',
-      mobileIcon:'/images/img_bicartplus.svg',
       bgColor: 'bg-primary-coral-light',
       bgcolorCollapsed:'#fde3e5',
       mobileTextColor: "text-[rgba(245,116,126,1)]",
     },
+    
   ];
+
+  
+const servicesData = isMobile ? servicesMobile : services;
+
   return (
 
             <section id="services" className="md:pt-[90px] pt-[60px]">
               <div className="container mx-auto px-4 overflow-hidden">
                 <h2 className=" font-[600] font-source text-center text-title md:mb-0 mb-3 title">Our Services</h2>
                 <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-x-4 gap-y-4 mx-auto our-services-wrapper">
-                  {
-                  
-                  services
-                  
-                  .map((service, index) => (
+
+                  {servicesData.map((service, index) => (
                     <div
                       key={service.id}
                       onClick={() => handleClick(index)}
@@ -183,8 +180,7 @@ const ServicesSection = () => {
                         className={`${service.bgColor} mobile-bg-transparent w-20 md:h-20 h-auto rounded-full flex items-center justify-center mx-auto md:mb-6 mb-4`}
                       >
 
-                        <Image src={service.icon} alt={service.title} width={36} height={36} className='md:block hidden'/>
-                        <Image src={service.mobileIcon} alt={service.title} width={36} height={36} className='md:hidden block'/>
+                        <Image src={service.icon} alt={service.title} width={36} height={36}/>
 
                       </div>
                       <h3 className={` subheading font-bold md:text-title md:mb-1 mb-0 md:h-auto h-[60px] md:leading-[normal] leading-[23px] content-center ${service.mobileTextColor}`}>{service.title}</h3>
