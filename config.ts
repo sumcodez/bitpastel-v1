bc096f0d1d912864e894a27b90ced2b817560e4c
sumcodez <cloud7suman@gmail.com>
2025-07-15 17:19:23 +0530

    careers partners

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 // config.ts
 const config = {
-  API_BASE_URL: 'https://www.bitpastel.com/api',
+//   API_BASE_URL: 'https://www.bitpastel.com/api',
+  API_BASE_URL: 'https://phpstack-1490991-5686871.cloudwaysapps.com/api',
 };
 
 export default config;
