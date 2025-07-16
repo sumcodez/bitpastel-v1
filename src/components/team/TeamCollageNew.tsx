554664201294d2befdfc1ac910290e3d5c664f53
Palash Purkait <palash1998purkait@gmail.com>
2025-07-16 11:03:52 +0530

    all pages changes design

--- src/components/team/TeamCollageNew.tsx
+++ src/components/team/TeamCollageNew.tsx
@@ -8,22 +8,22 @@ const TeamCollageNew = () => {
           <h2 className="text-title title font-[600]">Escape Rooms</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
             <img
-              src="/images/new-team-images/escape_room_img-1.webp"
+              src="/images/new-team-images/new/escape_room_img-1.webp"
               alt="escape room with team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/escape_room_img-2.webp"
+              src="/images/new-team-images/new/escape_room_img-2.webp"
               alt="escape room with team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/escape_room_img-3.webp"
+              src="/images/new-team-images/new/escape_room_img-3.webp"
               alt="escape room with team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/escape_room_img-4.webp"
+              src="/images/new-team-images/new/escape_room_img-4.webp"
               alt="escape room with team members"
               className="w-full object-top"
             />
@@ -37,22 +37,22 @@ const TeamCollageNew = () => {
           <h2 className="text-title title font-[600]">Interactive Team Mission</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
             <img
-              src="/images/new-team-images/jenga_puzzle-play-1.jpg"
+              src="/images/new-team-images/new/jenga_puzzle-play-1.webp"
               alt="Jenga Puzzle Playing team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/jenga_puzzle-play-2.webp"
+              src="/images/new-team-images/new/jenga_puzzle-play-2.webp"
               alt="Jenga Puzzle Playing team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/jenga_puzzle-play-3.webp"
+              src="/images/new-team-images/new/jenga_puzzle-play-3.webp"
               alt="Jenga Puzzle Playing team members"
               className="w-full"
             />
             <img
-              src="/images/new-team-images/jenga_puzzle-play-4.webp"
+              src="/images/new-team-images/new/jenga_puzzle-play-4.webp"
               alt="Jenga Puzzle Playing team members"
               className="w-full"
             />
