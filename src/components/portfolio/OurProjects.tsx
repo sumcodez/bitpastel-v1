6bc66875a3fbca070de710d96c46d47813441e14
Palash Purkait <palash1998purkait@gmail.com>
2025-07-17 14:10:32 +0530

    all pages changes design

--- src/components/portfolio/OurProjects.tsx
+++ src/components/portfolio/OurProjects.tsx
@@ -21,32 +21,253 @@ type Project = {
 };
 
 const OurProjects = () => {
+
+
   const projects: Project[] = [
-    {
-      id: 1,
-      title: 'MVD',
-      category: "Fashion",
-      link:"https://www.manieredevoir.com/",
-      images: [
-        '/images/project/MDV/mdv_mockup_V04D.webp',
-        '/images/project/MDV/mdv_mockup_V04C.webp',
-        '/images/project/MDV/mdv_mockup_V04B.webp',
-        '/images/project/MDV/mdv_mockup_V04A.webp',
-      ],
-      hoverImage: '/images/project/MDV/mdv_mockup_V04C.webp'
-    },
-    {
-      id: 2,
-      title: 'Circulr',
-      category: "Sunglasses & Eye Frames",
-      link:"https://circulr.co/",
-      images: [
-        '/images/project/Circulr/circulr_mockup_V03A.webp',
-        '/images/project/Circulr/circulr_mockup_V03B.webp',
-      ],
-      hoverImage: '/images/project/Circulr/circulr_mockup_V03B.webp'
-    },
-  ];
+  {
+    "id": 1,
+    "title": "MVD",
+    "category": "Fashion",
+    "link": "https://www.manieredevoir.com/",
+    "images": [
+      "/images/project/MDV/mdv_mockup_V04D.webp",
+      "/images/project/MDV/mdv_mockup_V04C.webp",
+      "/images/project/MDV/mdv_mockup_V04B.webp",
+      "/images/project/MDV/mdv_mockup_V04A.webp",
+    ],
+    "hoverImage": "/images/project/MDV/mdv_mockup_V04C.webp"
+  },
+  {
+    "id": 2,
+    "title": "Circulr",
+    "category": "Sunglasses & Eye Frames",
+    "link": "https://circulr.co/",
+    "images": [
+      "/images/project/Circulr/circulr_mockup_V03A.webp",
+      "/images/project/Circulr/circulr_mockup_V03B.webp"
+    ],
+    "hoverImage": "/images/project/Circulr/circulr_mockup_V03B.webp"
+  },
+  {
+    "id": 3,
+    "title": "The Couture Club",
+    "category": "Fashion",
+    "link": "https://www.thecoutureclub.com",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V01.webp",
+      "/images/project/Baee/Baee_mockup_V02.webp",
+      "/images/project/Baee/Baee_mockup_V03.webp",
+    ],
+    "hoverImage": "/images/project/TheCoutureClub/mockup1.webp"
+  },
+  {
+    "id": 4,
+    "title": "Mauvais",
+    "category": "Fashion",
+    "link": "https://mauvais.co.uk/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V04.webp",
+     "/images/project/Baee/Baee_mockup_V03.webp",
+    ],
+    "hoverImage": "/images/project/Mauvais/mockup1.webp"
+  },
+  {
+    "id": 5,
+    "title": "Amity Alexandra",
+    "category": "Jewellery",
+    "link": "https://amityalexandra.com",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V02.webp"
+    ],
+    "hoverImage": "/images/project/AmityAlexandra/mockup1.webp"
+  },
+  {
+    "id": 6,
+    "title": "Lovinity",
+    "category": "Jewellery",
+    "link": "https://lovinitydiamond.com",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V01.webp"
+    ],
+    "hoverImage": "/images/project/Lovinity/mockup1.webp"
+  },
+  {
+    "id": 7,
+    "title": "Auriga Jewelery",
+    "category": "Jewellery",
+    "link": "https://aurigajewelry.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V04.webp",
+      "/images/project/Baee/Baee_mockup_V01.webp"
+    ],
+    "hoverImage": "/images/project/AurigaJewelery/mockup1.webp"
+  },
+  {
+    "id": 8,
+    "title": "JZN Market",
+    "category": "Sim & Top-ups",
+    "link": "https://jznmarket.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V03.webp"
+    ],
+    "hoverImage": "/images/project/JZNMarket/mockup1.webp"
+  },
+  {
+    "id": 9,
+    "title": "Miss Mini",
+    "category": "Fashion",
+    "link": "https://missmini.com/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V01.webp",
+       "/images/project/Baee/Baee_mockup_V04.webp"
+    ],
+    "hoverImage": "/images/project/MissMini/mockup1.webp"
+  },
+  {
+    "id": 10,
+    "title": "Visual Displays",
+    "category": "Audio visual equipment",
+    "link": "https://visualdisplays.co.uk/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V03.webp"
+    ],
+    "hoverImage": "/images/project/VisualDisplays/mockup1.webp"
+  },
+  {
+    "id": 11,
+    "title": "Edit London",
+    "category": "Fashion",
+    "link": "https://www.theeditldn.com/en-intl",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V02.webp"
+    ],
+    "hoverImage": "/images/project/EditLondon/mockup1.webp"
+  },
+  {
+    "id": 12,
+    "title": "Roofing Outlet",
+    "category": "Building supplies",
+    "link": "https://www.roofingoutlet.co.uk/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V04.webp"
+    ],
+    "hoverImage": "/images/project/RoofingOutlet/mockup1.webp"
+  },
+  {
+    "id": 13,
+    "title": "Kite Beauty",
+    "category": "Beauty",
+    "link": "https://kitebeauty.co/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V01.webp",
+      "/images/project/KiteBeauty/mockup2.webp"
+    ],
+    "hoverImage": "/images/project/KiteBeauty/mockup1.webp"
+  },
+  {
+    "id": 14,
+    "title": "Pepa London",
+    "category": "Fashion - Kids",
+    "link": "https://pepalondon.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V02.webp"
+    ],
+    "hoverImage": "/images/project/PepaLondon/mockup1.webp"
+  },
+  {
+    "id": 15,
+    "title": "Kaiia",
+    "category": "Fashion",
+    "link": "https://www.kaiiathelabel.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V03.webp"
+    ],
+    "hoverImage": "/images/project/Kaiia/mockup1.webp"
+  },
+  {
+    "id": 16,
+    "title": "Public Desire",
+    "category": "Fashion",
+    "link": "https://www.publicdesire.com/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V04.webp"
+    ],
+    "hoverImage": "/images/project/PublicDesire/mockup1.webp"
+  },
+  {
+    "id": 17,
+    "title": "A Man & His Cave",
+    "category": "Online entertainment & furnishing retailer",
+    "link": "https://amanandhiscave.com/",
+    "images": [
+     "/images/project/Baee/Baee_mockup_V03.webp",
+      "/images/project/Baee/Baee_mockup_V01.webp"
+    ],
+    "hoverImage": "/images/project/AManAndHisCave/mockup1.webp"
+  },
+  {
+    "id": 18,
+    "title": "Spring Copanhagen",
+    "category": "Gifts",
+    "link": "https://en.springcopenhagen.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V02.webp",
+       "/images/project/Baee/Baee_mockup_V04.webp"
+    ],
+    "hoverImage": "/images/project/SpringCopenhagen/mockup1.webp"
+  },
+  {
+    "id": 19,
+    "title": "Flask Fine Wines",
+    "category": "Alcoholic beverages",
+    "link": "https://flaskfinewines.com/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V03.webp"
+    ],
+    "hoverImage": "/images/project/FlaskFineWines/mockup1.webp"
+  },
+  {
+    "id": 20,
+    "title": "Embassy London",
+    "category": "Fashion",
+    "link": "https://embassylondon.co.uk/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V01.webp"
+    ],
+    "hoverImage": "/images/project/EmbassyLondon/mockup1.webp"
+  },
+  {
+    "id": 21,
+    "title": "Tinted Fashion",
+    "category": "Fashion",
+    "link": "https://tintedfashion.be/",
+    "images": [
+       "/images/project/Baee/Baee_mockup_V04.webp"
+    ],
+    "hoverImage": "/images/project/TintedFashion/mockup1.webp"
+  },
+  {
+    "id": 22,
+    "title": "Rokoko",
+    "category": "Motion-capture hardware and software",
+    "link": "https://store.rokoko.com/",
+    "images": [
+      "/images/project/Baee/Baee_mockup_V02.webp"
+    ],
+    "hoverImage": "/images/project/Rokoko/mockup1.webp"
+  },
+  {
+    "id": 23,
+    "title": "Warm Nordic",
+    "category": "Designed furniture, lighting, and home accessories",
+    "link": "https://warmnordic.com/en",
+    "images": [
+     "/images/project/Baee/Baee_mockup_V01.webp"
+    ],
+    "hoverImage": "/images/project/WarmNordic/mockup1.webp"
+  }
+];
+
 
   const allCategories: string[] = projects.map((project: Project) => project.category);
   const uniqueCategories: string[] = Array.from(new Set(allCategories));
@@ -127,9 +348,10 @@ const OurProjects = () => {
                 >
                   <div className="project-image relative h-[300px] overflow-hidden">
                     <div className="absolute inset-0">
-                      <div className={`absolute inset-0 image-transition ${
+                      <div className={`absolute inset-0 image-transition`}>
+                      {/* <div className={`absolute inset-0 image-transition ${
                         hoveredProject === project.id && project.hoverImage ? 'opacity-0' : 'opacity-100'
-                      }`}>
+                      }`}> */}
                         <Image 
                           src={project.images[0]} 
                           alt={`${project.title} cover image`}
@@ -138,7 +360,7 @@ const OurProjects = () => {
                           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                         />
                       </div>
-                      {project.hoverImage && (
+                      {/* {project.hoverImage && (
                         <div className={`absolute inset-0 image-transition ${
                           hoveredProject === project.id ? 'opacity-100' : 'opacity-0'
                         }`}>
@@ -150,15 +372,15 @@ const OurProjects = () => {
                             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                           />
                         </div>
-                      )}
+                      )} */}
                     </div>
                   </div>
                   <div className="text-center pt-12 pb-16">
-                   <Link href={project.link} className="">
-                      <h3 className="text-title subheading font-source font-[700] hover:text-green-600 transition-colors">
+                   {/* <Link href={project.link} className=""> */}
+                      <h3 className="text-title subheading font-source font-[700]">
                         {project.title}
                       </h3>
-                    </Link>
+                    {/* </Link> */}
                     <button
                       onClick={() => openModal(project)}
                       className="min-w-[96px] card-button rounded-[4px] bg-green-btn text-primary-white py-2 px-5 w-auto mx-auto hover:bg-opacity-90 transition-opacity button-hover"
@@ -183,12 +405,12 @@ const OurProjects = () => {
           <div className={`bg-white rounded-lg max-w-6xl w-full max-h-[90vh] flex flex-col scale-in`}>
             <div className="flex justify-between items-center border-b p-4">
               <div>
-                <h3 className="text-2xl font-bold text-gray-800">{currentProject.title}</h3>
-                <p className="text-gray-600">{currentProject.category}</p>
+                <h3 className="text-title titile font-bold ">{currentProject.title}</h3>
+                <p className="text-dark">{currentProject.category}</p>
               </div>
               <button
                 onClick={closeModal}
-                className="text-gray-500 hover:text-gray-700 p-1 rounded-full hover:bg-gray-100 transition-colors button-hover"
+                className="text-dark hover:text-title p-1 rounded-full hover:bg-gray-100 transition-colors button-hover"
                 aria-label="Close modal"
               >
                 <svg
@@ -221,7 +443,7 @@ const OurProjects = () => {
                 >
                   {currentProject.images.map((image: string, index: number) => (
                     <SwiperSlide key={index}>
-                      <div className="flex justify-center items-center h-full w-full relative">
+                       <div className="flex justify-center items-center h-full w-full relative">
                         <Image
                           src={image}
                           alt={`${currentProject.title} - ${index + 1}`}
@@ -237,11 +459,11 @@ const OurProjects = () => {
 
               {/* Thumbs Swiper */}
               {currentProject.images.length > 1 && (
-                <div className="mt-2 p-2 rounded-lg relative">
+                <div className="mt-2 p-2 rounded-lg thubnail-slider relative">
                   <Swiper
                     modules={[FreeMode, Navigation, Thumbs]}
                     onSwiper={setThumbsSwiper}
-                    spaceBetween={8}
+                    spaceBetween={10}
                     slidesPerView={Math.min(currentProject.images.length, 4)}
                     freeMode={true}
                     watchSlidesProgress={true}
@@ -250,10 +472,10 @@ const OurProjects = () => {
                     {currentProject.images.map((image: string, index: number) => (
                       <SwiperSlide key={index} className="cursor-pointer">
                         <div 
-                          className={`flex justify-center items-center h-20 bg-white rounded border overflow-hidden relative transition-all duration-200 thumb-hover ${
+                          className={`flex justify-center items-center h-20 bg-white rounded overflow-hidden relative transition-all duration-200 thumb-hover ${
                             activeThumbIndex === index 
-                              ? 'opacity-100 border-green-500 border-2' 
-                              : 'opacity-50 border-gray-200'
+                              ? 'active-slide' 
+                              : ''
                           }`}
                         >
                           <Image
