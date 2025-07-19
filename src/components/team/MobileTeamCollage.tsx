d7fcd6350e1ea6314e7bb885244be08c8d6a69d3
Palash Purkait <palash1998purkait@gmail.com>
2025-07-19 18:55:53 +0530

    all pages changes design

--- src/components/team/MobileTeamCollage.tsx
+++ src/components/team/MobileTeamCollage.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
