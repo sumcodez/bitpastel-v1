5b166e8f195e2fd3bfb440fc20d08302f0e06483
sumcodez <cloud7suman@gmail.com>
2025-07-26 18:38:39 +0530

    our services 5 sec

--- src/components/home/ServicesSection.tsx
+++ src/components/home/ServicesSection.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
-import Image from 'next/image'
+import React, { useEffect } from 'react';
+import Image from 'next/image';
 import { useState } from 'react';
 
-interface services{
- id: string;
+interface services {
+  id: string;
   title: string;
   description: string;
   icon: string;
@@ -12,37 +12,49 @@ interface services{
   mobileTextColor: string;
 }
 
-
 const ServicesSection = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
-  const [isMobile, setisMobile] = useState(false); 
-
+  const [isMobile, setisMobile] = useState(false);
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
-  const handleResize = () => {
-    setisMobile(window.innerWidth <= 767);
-  };
-  handleResize();
-  window.addEventListener('resize', handleResize);
-  return () => {
-    window.removeEventListener('resize', handleResize);
-  };
-})
-
-
+    const handleResize = () => {
+      setisMobile(window.innerWidth <= 767);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
 
   const handleClick = (index: number) => {
-    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
-  }; 
-      const services:services[] = [
+    // setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+        // Clear any existing timeout
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    
+    // Set the new active index
+    setActiveIndex(index);
+    
+    // Set a timeout to remove the active class after 5 seconds
+    const newTimeoutId = setTimeout(() => {
+      setActiveIndex(null);
+    }, 5000);
+    
+    setTimeoutId(newTimeoutId);
+  };
+  
+  const services: services[] = [
     {
       id: 'ai',
       title: 'Artificial Intelligence',
       description: 'Smart AI agents and solutions driving automation, insight, and innovation',
       icon: '/images/AI icon 1.svg',
       bgColor: '  bg-primary-green-light',
-      bgcolorCollapsed:'#fcf4d7',
-      mobileTextColor: " text-[rgba(240,198,56,1)]",
+      bgcolorCollapsed: '#fcf4d7',
+      mobileTextColor: ' text-[rgba(240,198,56,1)]',
     },
     {
       id: 'shopify',
@@ -51,8 +63,7 @@ const ServicesSection = () => {
       icon: '/images/img_hugeiconsshopify.svg',
       bgColor: 'bg-primary-yellow-light',
       bgcolorCollapsed: '#d8f5e9',
-      mobileTextColor: "text-[rgba(62,203,144,1)]",
-      
+      mobileTextColor: 'text-[rgba(62,203,144,1)]',
     },
     {
       id: 'web',
@@ -60,8 +71,8 @@ const ServicesSection = () => {
       description: 'Robust, responsive web apps built for performance and user experience',
       icon: '/images/img_icoutlinelaptop.svg',
       bgColor: 'bg-primary-coral-light',
-      bgcolorCollapsed:'#fde3e5',
-       mobileTextColor: "text-[rgba(245,116,126,1)]",
+      bgcolorCollapsed: '#fde3e5',
+      mobileTextColor: 'text-[rgba(245,116,126,1)]',
     },
     {
       id: 'design',
@@ -69,8 +80,8 @@ const ServicesSection = () => {
       description: 'Engaging, user-focused designs that bring your vision to life',
       icon: '/images/img_iconoirdesignnib.svg',
       bgColor: 'bg-primary-yellow-light',
-      bgcolorCollapsed:'#fcf4d7',
-      mobileTextColor: "text-[rgba(240,198,56,1)]",
+      bgcolorCollapsed: '#fcf4d7',
+      mobileTextColor: 'text-[rgba(240,198,56,1)]',
     },
     {
       id: 'ecommerce',
@@ -78,8 +89,8 @@ const ServicesSection = () => {
       description: 'End-to-end e-commerce solutions for seamless shopping and sales growth',
       icon: '/images/img_bicartplus.svg',
       bgColor: 'bg-primary-coral-light',
-      bgcolorCollapsed:'#fde3e5',
-      mobileTextColor: "text-[rgba(245,116,126,1)]",
+      bgcolorCollapsed: '#fde3e5',
+      mobileTextColor: 'text-[rgba(245,116,126,1)]',
     },
     {
       id: 'saas',
@@ -87,22 +98,20 @@ const ServicesSection = () => {
       description: 'Tailored SaaS platforms that solve real problems and scale faster',
       icon: '/images/img_lineiconscloudiot2.svg',
       bgColor: 'bg-primary-green-light',
-      bgcolorCollapsed:'#d8f5e9',
-      mobileTextColor: "text-[rgba(62,203,144,1)]",
+      bgcolorCollapsed: '#d8f5e9',
+      mobileTextColor: 'text-[rgba(62,203,144,1)]',
     },
   ];
 
-
-
-      const servicesMobile:services[] = [
+  const servicesMobile: services[] = [
     {
       id: 'ai',
       title: 'Artificial Intelligence',
       description: 'Smart AI agents and solutions driving automation, insight, and innovation',
       icon: '/images/Our services icons/ai-yellow-icon.svg',
       bgColor: ' bg-primary-yellow-light ',
-      bgcolorCollapsed:'#fcf4d7',
-      mobileTextColor: " text-[rgba(240,198,56,1)] ",
+      bgcolorCollapsed: '#fcf4d7',
+      mobileTextColor: ' text-[rgba(240,198,56,1)] ',
     },
     {
       id: 'shopify',
@@ -111,8 +120,7 @@ const ServicesSection = () => {
       icon: '/images/Our services icons/shopify-green-icon.svg',
       bgColor: 'bg-primary-green-light',
       bgcolorCollapsed: '#d8f5e9',
-      mobileTextColor: "text-[rgba(62,203,144,1)]",
-      
+      mobileTextColor: 'text-[rgba(62,203,144,1)]',
     },
     {
       id: 'web',
@@ -120,8 +128,8 @@ const ServicesSection = () => {
       description: 'Robust, responsive web apps built for performance and user experience',
       icon: '/images/Our services icons/web_developement_icon.svg',
       bgColor: 'bg-primary-coral-light',
-      bgcolorCollapsed:'#fde3e5',
-       mobileTextColor: "text-[rgba(245,116,126,1)]",
+      bgcolorCollapsed: '#fde3e5',
+      mobileTextColor: 'text-[rgba(245,116,126,1)]',
     },
     {
       id: 'design',
@@ -129,8 +137,8 @@ const ServicesSection = () => {
       description: 'Engaging, user-focused designs that bring your vision to life',
       icon: '/images/Our services icons/design_creative_icon.svg',
       bgColor: 'bg-primary-yellow-light',
-      bgcolorCollapsed:'#fcf4d7',
-      mobileTextColor: "text-[rgba(240,198,56,1)]",
+      bgcolorCollapsed: '#fcf4d7',
+      mobileTextColor: 'text-[rgba(240,198,56,1)]',
     },
     {
       id: 'saas',
@@ -138,9 +146,8 @@ const ServicesSection = () => {
       description: 'Tailored SaaS platforms that solve real problems and scale faster',
       icon: '/images/Our services icons/saas_developement.svg',
       bgColor: 'bg-primary-green-light',
-      bgcolorCollapsed:'#d8f5e9',
-      mobileTextColor: "text-[rgba(62,203,144,1)]",
-
+      bgcolorCollapsed: '#d8f5e9',
+      mobileTextColor: 'text-[rgba(62,203,144,1)]',
     },
     {
       id: 'ecommerce',
@@ -148,63 +155,63 @@ const ServicesSection = () => {
       description: 'End-to-end e-commerce solutions for seamless shopping and sales growth',
       icon: '/images/Our services icons/img_bicartplus.svg',
       bgColor: 'bg-primary-coral-light',
-      bgcolorCollapsed:'#fde3e5',
-      mobileTextColor: "text-[rgba(245,116,126,1)]",
+      bgcolorCollapsed: '#fde3e5',
+      mobileTextColor: 'text-[rgba(245,116,126,1)]',
     },
-    
   ];
 
-  
-const servicesData = isMobile ? servicesMobile : services;
+  const servicesData = isMobile ? servicesMobile : services;
 
   return (
-
-            <section id="services" className="md:pt-[90px] pt-[60px]">
-              <div className="container mx-auto px-4 overflow-hidden">
-                <h2 className=" md:font-[600] font-[700] font-source text-center text-title md:mb-0 mb-3 title">Our Services</h2>
-                <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-x-4 gap-y-4 mx-auto our-services-wrapper">
-
-                  {servicesData.map((service, index) => (
-                    <div
-                      key={service.id}
-                      onClick={() => handleClick(index)}
-                      // className="text-center hover:shadow-lg transition-shadow duration-300 cursor-pointer py-10"
-                      className={`card-element text-center transition-shadow duration-300m md:min-w-[275px] min-w-[unset] cursor-pointer md:py-8 pt:0 pb-8 md:bg-transparent ${service.bgColor} md:rounded rounded-[10px] md:min-h-full min-h-[160px]
-                        ${
-            activeIndex === index ? 'active' : ''
-          }
+    <section id="services" className="md:pt-[90px] pt-[60px]">
+      <div className="container mx-auto px-4 overflow-hidden">
+        <h2 className=" md:font-[600] font-[700] font-source text-center text-title md:mb-0 mb-3 title">
+          Our Services
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-3 md:grid-cols-2 gap-x-4 gap-y-4 mx-auto our-services-wrapper">
+          {servicesData.map((service, index) => (
+            <div
+              key={service.id}
+              onClick={() => handleClick(index)}
+              // className="text-center hover:shadow-lg transition-shadow duration-300 cursor-pointer py-10"
+              className={`card-element text-center transition-shadow duration-300m md:min-w-[275px] min-w-[unset] cursor-pointer md:py-8 pt:0 pb-8 md:bg-transparent ${service.bgColor} md:rounded rounded-[10px] md:min-h-full min-h-[160px]
+                        ${activeIndex === index ? 'active' : ''}
                         `}
-                    >
-                      <div className='mobile-none'>
-                      <div
-                        className={`${service.bgColor} mobile-bg-transparent w-20 md:h-20 h-auto rounded-full flex items-center justify-center mx-auto md:mb-6 mb-4`}
-                      >
+            >
+              <div className="mobile-none">
+                <div
+                  className={`${service.bgColor} mobile-bg-transparent w-20 md:h-20 h-auto rounded-full flex items-center justify-center mx-auto md:mb-6 mb-4`}
+                >
+                  <Image src={service.icon} alt={service.title} width={36} height={36} />
+                </div>
+                <h3
+                  className={`font-[500] md:font-bold md:text-title md:mb-2 mb-0 md:h-auto h-[60px] md:leading-[normal] md:text-[20px] text-[16px] leading-[20px] md:px-0 px-[17px]  content-center ${service.mobileTextColor}`}
+                >
+                  {service.title}
+                </h3>
+                <p className="-relaxed paragraph md:max-w-[275px] max-w-[unset] w-full mx-auto text-title">
+                  {service.description}
+                </p>
+              </div>
+              <div
+                className={`collapsible-element text-left content-center${activeIndex === index ? 'active' : ''}`}
+                style={{ background: service.bgcolorCollapsed }}
+              >
+                <p className="paragraph text-title text-[16px] leading-[20px]">
+                  {service.description}
+                </p>
+              </div>
 
-                        <Image src={service.icon} alt={service.title} width={36} height={36}/>
-
-                      </div>
-                      <h3 className={`font-[500] md:font-bold md:text-title md:mb-2 mb-0 md:h-auto h-[60px] md:leading-[normal] md:text-[20px] text-[16px] leading-[20px] md:px-0 px-[17px]  content-center ${service.mobileTextColor}`}>{service.title}</h3>
-                      <p className="-relaxed paragraph md:max-w-[275px] max-w-[unset] w-full mx-auto text-title">
-                        {service.description}
-                      </p>
-                      </div>
-                      <div className={`collapsible-element text-left content-center${activeIndex === index ? 'active' : ''}`} style={{background:service.bgcolorCollapsed}}>
-                        <p className='paragraph text-title text-[16px] leading-[20px]'>
-                          {service.description}
-                        </p>
-                      </div>
-
-                      {/* <div className='mobile-block'>
+              {/* <div className='mobile-block'>
                            <h3 className={` font-bold ${service.mobileTextColor} mb-3 font-source leading-[20px]`}>{service.title}</h3>
                            <p className='text-[rgba(59,59,61,1)] text-[12px] leading-[20px]'>{service.description}</p>
                       </div> */}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </section>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-  )
-}
-
-export default ServicesSection
+export default ServicesSection;
