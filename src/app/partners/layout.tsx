38dfc47379ffad64901ab938a87a61229ac74901
Palash Purkait <palash1998purkait@gmail.com>
2025-07-31 16:03:55 +0530

    all pages changes design

--- src/app/partners/layout.tsx
+++ src/app/partners/layout.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 export const metadata: Metadata = {
- title: 'Partnership Program | Bitpastel - Collaborate for E-commerce Success',
+ title: ' Bitpastel | Partnership Program - Collaborate for E-commerce Success',
   description: 'Join Bitpastel as a partner and unlock new business opportunities in Shopify Plus development, e-commerce solutions, and digital transformation services. Grow together with our technology partnership program.',
   icons: {
     icon: [{ url: '/favicon.ico', sizes: 'any', type: 'image/x-icon' }],
